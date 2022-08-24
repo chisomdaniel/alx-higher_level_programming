7d791a752a2a8743c9c8331e5845b3d724c997d4
Chisom Daniel <chinwezechisom@gmail.com>
2022-08-24 01:15:31 +0100

    Python files

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def remove_char_at(str, n):
+    string = ''
+    for i in range(len(str)):
+        if i == n:
+            continue
+        string += str[i]
+    return string
