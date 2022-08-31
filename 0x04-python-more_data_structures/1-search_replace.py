b28829ffe0e69dd0f54cdaaf45b2e2ea73139351
Chisom Daniel <chinwezechisom@gmail.com>
2022-09-01 00:01:29 +0100

    changes

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
-    
+
+    length = len(my_list)
+
+    if search >= length:
+        return my_list
+
     new_list = my_list.copy()
     new_list[search - 1] = replace
 
