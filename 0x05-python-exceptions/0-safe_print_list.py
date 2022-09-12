fbd798e9fd11c57c4eb1c060729ec53efb83ee51
Chisom Daniel <chinwezechisom@gmail.com>
2022-09-12 15:08:10 +0100

    error

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,6 +6,7 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print(my_list[i], end="")
             count += 1
-    except:
-        print("")
-        return count
+    except IndexError:
+        pass
+    print("")
+    return count
