36c2e556e23a2bcb450b9c4c982b3c40ec22771b
Chisom Daniel <chinwezechisom@gmail.com>
2022-09-12 15:16:03 +0100

    exception

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,7 +3,7 @@
 def safe_print_division(a, b):
     try:
         answer = a/b
-    except:
+    except ZeroDivisionError:
         answer = None
     finally:
         print("Inside result: {}".format(answer))
