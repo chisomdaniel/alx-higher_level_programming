6e4983e6c8d7a64bf095a77c635a71e031d42026
Chisom Daniel <chinwezechisom@gmail.com>
2022-09-12 15:20:36 +0100

    exception

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -11,7 +11,7 @@ def list_division(my_list_1, my_list_2, list_length):
         except ZeroDivisionError:
             print("division by 0")
             newlist.append(0)
-        except:
+        except (TypeError, ValueError):
             print("wrong type")
             newlist.append(0)
         finally:
