3bc049e454351e1697db79775346cd954bb0d9b2
Chisom Daniel <chinwezechisom@gmail.com>
2022-09-13 23:08:03 +0100

    added lines

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""This file contain a test python class
+
+It has no attribute or obje
+
+"""
+
+
+class Square:
+    """This class defines a square
+
+    Attributes:
+        size (private): is the size of the square.
+
+    """
+
+    def __init__(self, size):
+        self.__size = size
