ed396399cbe3eb1a696b629d2725a54ba8c24c8b
Chisom Daniel <chinwezechisom@gmail.com>
2022-09-13 23:50:25 +0100

    more methods

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+"""This file contain a test python class
+
+It has no attribute or obje
+
+"""
+
+
+class Square:
+    """This class defines a square
+
+    Attributes:
+        size (private): is the size of the square.
+
+    """
+
+    def __init__(self, size=0):
+
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    @property
+    def size(self):
+        """size: `int` is retrived using the method
+        and also set using it's setter method
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """ A public method that returns area of the square"""
+        return self.__size ** 2
+
+    def my_print(self):
+        """Represents the square with the # symbol"""
+        if self.__size == 0:
+            print("")
+            return
+        for i in range(self.__size):
+            print("{}".format("#"*self.__size))
+
+
+
+
+
+
+
