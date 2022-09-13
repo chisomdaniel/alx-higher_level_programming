537331a666861321b13776b285a97e27753fec81
Chisom Daniel <chinwezechisom@gmail.com>
2022-09-13 22:55:42 +0100

    classes

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """This file contain a test python class
 
-It has no attribute or object
+It has no attribute or obje
 
 """
 
+
 class Square:
     """This is an empty class"""
+
     pass
