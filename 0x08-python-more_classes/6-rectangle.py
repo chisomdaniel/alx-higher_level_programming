45c30493b5d1a0978acf443aa61d2ecd5fbf7073
Chisom Daniel <chinwezechisom@gmail.com>
2022-09-20 00:22:08 +0100

    no

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -10,7 +10,7 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
-        number_of_instances += 1
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
@@ -73,4 +73,4 @@ class Rectangle:
     def __del__(self):
         """Print this when an instance is deleted"""
         print("Bye rectangle...")
-        number_of_instances -= 1
+        Rectangle.number_of_instances -= 1
