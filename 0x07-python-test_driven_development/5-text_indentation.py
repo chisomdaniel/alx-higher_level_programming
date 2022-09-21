f81844c6e0ea28aba2838d7beb254a306a06fb86
Chisom Daniel <chinwezechisom@gmail.com>
2022-09-21 01:37:48 +0100

    more test

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,30 +1,28 @@
 #!/usr/bin/python3
+"""A module that parses and print a string
 
+Function: text_indentation
+
+"""
 def text_indentation(text):
+    """The function to print the string
+
+    Arguments:
+        text (str): the string to parse
+    """
 
     if type(text) != str:
         raise TypeError("text must be a string")
 
-    text_list = text.split(".")
-
-    text_list_2 = []
-    text_list_3 = []
-    for i in text_list:
-        text_list_2 += i.split("?")
-    for i in text_list_2:
-        text_list_3 += i.split(":")
-
-    for i in text_list_3:
-        new_text_list = i.strip()
-        print(new_text_list,  "\n")
-
-
-text_indentation("""Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
-        Quonam modo? Utrum igitur tibi litteram videor an totas paginas commovere? \
-        Non autem hoc: igitur ne illud quidem. Fortasse id optimum, sed ubi illud: \
-        Plus semper voluptatis? Teneo, inquit, finem illi videri nihil dolere. \
-        Transfer idem ad modestiam vel temperantiam, quae est moderatio cupiditatum \
-        rationi oboediens. Si id dicis, vicimus. Inde sermone vario sex illa a Dipylo \
-        stadia confecimus. Sin aliud quid voles, postea. Quae animi affectio suum \
-        cuique tribuens atque hanc, quam dico. Utinam quidem dicerent alium alio \
-        beatiorem! Iam ruinas videres""")
+    split = False
+    for i in text:
+        if i in (".", ":", "?"):
+            print(i)
+            print("")
+            split = True
+            continue
+        if split and i in (" ", "\t"):
+            continue
+        else:
+            split = False
+        print(i, end="")
