6bdb58dc3e392d92e0d74c561c86ca55d399d7d5
Chisom Daniel <chinwezechisom@gmail.com>
2022-09-29 16:59:10 +0100

    fixed doc

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -7,7 +7,7 @@ or indirectly) from the specified class ; otherwise False
 
 
 def inherits_from(obj, a_class):
-    
+    """returns True if true, else False"""    
     if isinstance(obj, a_class):
         if type(obj) == a_class:
             return False
