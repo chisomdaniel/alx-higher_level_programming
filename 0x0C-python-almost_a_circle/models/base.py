efd9edb568cbf0a15a4f5c767c3921851f617b27
Chisom Daniel <chinwezechisom@gmail.com>
2022-10-08 02:21:37 +0100

    json

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """A python module that initialize a base class
 """
+import json
 
 
 class Base():
@@ -18,3 +19,12 @@ class Base():
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """returns the json string representation of
+        a list of dictionaries passed as an argument
+        """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+
+        return json.dumps(list_dictionaries)
