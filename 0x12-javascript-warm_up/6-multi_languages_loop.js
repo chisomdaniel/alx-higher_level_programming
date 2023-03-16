ddf7f7490a90fe9ef85969d7c16498668d9d9fe9
Chisom Daniel <chinwezechisom@gmail.com>
2023-03-16 22:01:15 +0100

    task 6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const list = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let count = 0;
+
+while (count <= 2) {
+  console.log(list[count]);
+  count = count + 1;
+}
