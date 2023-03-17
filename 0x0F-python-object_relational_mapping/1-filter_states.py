c006e8c2b08e26a2bfdadf10054af3629133589a
Chisom Daniel <chinwezechisom@gmail.com>
2023-03-17 23:23:47 +0100

    task 1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''Connecting a database with python'''
+import MySQLdb
+import sys
+
+
+if __name__ == '__main__':
+    '''Dont work on import'''
+
+    args = sys.argv
+
+    if (len(args) <= 3):
+        exit()
+
+    db = MySQLdb.connect(host='localhost', user=args[1],
+                         passwd=args[2], db=args[3])
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states WHERE name like \"N%\" ORDER BY id")
+    rows = cur.fetchall()
+
+    for i in rows:
+        print(i)
