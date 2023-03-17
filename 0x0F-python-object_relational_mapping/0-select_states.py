328ae1b0b603b54cf239fac2e3a4aef4cb357274
Chisom Daniel <chinwezechisom@gmail.com>
2023-03-17 22:20:00 +0100

    task 0

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''Connecting a database with python'''
+import MySQLdb
+import sys
+
+
+args = sys.argv
+
+if (len(args) <= 3):
+    exit()
+
+db = MySQLdb.connect(host='localhost', user=args[1],
+                     passwd=args[2], db=args[3])
+cur = db.cursor()
+
+cur.execute("SELECT * FROM states ORDER BY id")
+rows = cur.fetchall()
+
+for i in rows:
+    print(i)
