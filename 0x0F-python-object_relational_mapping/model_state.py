fe6d7b42105ae58d7621dd1434e1a7a9a83b4f1b
Chisom Daniel <chinwezechisom@gmail.com>
2023-03-18 15:40:04 +0100

    task 6

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''Connecting python to DATABASE using SQLALchemy'''
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy import Sequence
+
+
+Base = declarative_base()
+
+class State(Base):
+    '''A class for our state table'''
+
+    __tablename__ = "states"
+
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
