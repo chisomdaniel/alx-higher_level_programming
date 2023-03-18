a8bd2252be00bcda580881c5c3854d772ec737cb
Chisom Daniel <chinwezechisom@gmail.com>
2023-03-18 18:25:34 +0100

    task 8, a really good one

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+'''This script list all `State` objects from the database'''
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy import Sequence
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+import sys
+
+
+if (__name__ == "__main__"):
+    if (len(sys.argv) <= 3):
+        exit()
+
+    string = 'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3])
+    engine = create_engine(string, pool_pre_ping=True)
+
+    # Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    instance = session.query(State).order_by(State.id).first()
+    try:
+        print("{}: {}".format(instance.id, instance.name))
+    except AttributeError:
+        print("Nothing")
