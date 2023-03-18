2b0bee75cd3712a1ddeb5bbe64d63a5fddd0e8f0
Chisom Daniel <chinwezechisom@gmail.com>
2023-03-18 16:57:08 +0100

    task 7

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -10,16 +10,17 @@ from model_state import Base, State
 import sys
 
 
-if (len(sys.argv) <= 3 or __name__ != "__main__"):
-    exit()
+if (__name__ == "__main__"):
+    if (len(sys.argv) <= 3):
+        exit()
 
-string = 'mysql+mysqldb://{}:{}@localhost/{}'.format(
-        sys.argv[1], sys.argv[2], sys.argv[3])
-engine = create_engine(string, pool_pre_ping=True)
+    string = 'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3])
+    engine = create_engine(string, pool_pre_ping=True)
 
-# Base.metadata.create_all(engine)
-Session = sessionmaker(bind=engine)
-session = Session()
+    # Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-for instance in session.query(State).order_by(State.id):
-    print("{}: {}".format(instance.id, instance.name))
+    for instance in session.query(State).order_by(State.id):
+        print("{}: {}".format(instance.id, instance.name))
