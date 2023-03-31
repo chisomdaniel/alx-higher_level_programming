169f2e01a92afed5cc29529d51741c740ab435c6
Chisom Daniel <chinwezechisom@gmail.com>
2023-03-31 23:32:55 +0100

    new

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,6 +3,9 @@
 import urllib.request
 
 
+if __name__ != "__main__":
+    exit()
+
 with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
     html = response.read()
     print("Body response:")
