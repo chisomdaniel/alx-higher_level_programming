28fffc57642b0bd98ee6ae7666adbeb8c28f2db4
Chisom Daniel <chinwezechisom@gmail.com>
2023-04-01 21:06:53 +0100

    task 7, unchecked

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''fetches a url, using the `request` lib '''
+import requests
+import sys
+
+
+if __name__ == '__main__':
+
+    r = requests.get(sys.argv[1])
+
+    if r.status_code >= 400:
+        print("Error code:", r.status_code)
