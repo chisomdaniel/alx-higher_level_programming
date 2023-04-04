bc0de7309411fe3449c91f9ae6fee2668e1cc9ea
Chisom Daniel <chinwezechisom@gmail.com>
2023-04-04 23:40:47 +0100

    new revised

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/bash
-# sends a request to a URL and display the size of
-# the body of the response in bytes
-
+# sends a request to a URL and display the size of in bytes
 curl -s -w %{size_download} 0.0.0.0:5000 -o good ; echo ''
