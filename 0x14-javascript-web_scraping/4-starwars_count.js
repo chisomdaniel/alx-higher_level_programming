1317f1dea4322ddbad4082c4860c22b3ab8b49e3
Chisom Daniel <chinwezechisom@gmail.com>
2023-05-24 22:17:13 +0100

    semistandard

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -5,7 +5,6 @@ const request = require('request');
 const args = process.argv;
 
 const url = args[2];
-const character = 'https://swapi-api.alx-tools.com/api/people/18/';
 let count = 0;
 
 request(url, { json: true }, function (error, response, body) {
@@ -16,9 +15,9 @@ request(url, { json: true }, function (error, response, body) {
 
   for (let i = 0; i < body.results.length; i++) {
     for (let j = 0; j < body.results[i].characters.length; j++) {
-	if (body.results[i].characters[j].includes('18')) {
-          count++;
-	}
+      if (body.results[i].characters[j].includes('18')) {
+        count++;
+      }
     }
   }
 
