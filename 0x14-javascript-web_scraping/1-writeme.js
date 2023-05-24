506455566e32d8aa267ebf90d8b7c9218d1c2f75
Chisom Daniel <chinwezechisom@gmail.com>
2023-05-24 20:50:01 +0100

    task 1

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+const args = process.argv;
+const text = args[3];
+
+fs.writeFile(args[2], text, 'utf8', (err) => {
+	if (err) {
+		console.error(err);
+		return;
+	}
+});
