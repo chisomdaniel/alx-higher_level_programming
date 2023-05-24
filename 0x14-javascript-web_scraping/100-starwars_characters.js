afef0634bf11b49b72a737541a2b9018fcb10c93
Chisom Daniel <chinwezechisom@gmail.com>
2023-05-24 23:14:35 +0100

    advanced

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const args = process.argv;
+
+const url = 'https://swapi-api.alx-tools.com/api/films/' + args[2];
+
+request(url, { json: true }, function (error, response, body) {
+  if (error) {
+    console.error('error:', error);
+    return;
+  }
+
+  for (let j = 0; j < body.characters.length; j++) {
+    request(body.characters[j], { json: true }, function (err, response, body) {
+      if (err) {
+        return;
+      }
+      console.log(body.name);
+    });
+  }
+});
